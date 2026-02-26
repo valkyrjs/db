fe0913873fee85c7ef21bf3f6bfb7fea8561ab12
kodemon <hi@kodemon.net>
2026-02-26 12:50:32 +0100

    feat: export registrar type

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,6 @@
 export * from "./collection.ts";
 export * from "./databases/indexeddb/database.ts";
 export * from "./databases/memory/database.ts";
+export type { Registrars } from "./registrars.ts";
 export * from "./storage.ts";
 export type { AnyDocument } from "./types.ts";
